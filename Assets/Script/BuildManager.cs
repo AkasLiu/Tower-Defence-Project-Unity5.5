24114b2c1295f834c466b1e35ff2a3c11e77d293
ARCUS <929491690@qq.com>
2018-03-07 20:54:12 +0800

    video 15

---
+++ Assets/Script/BuildManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BuildManager : MonoBehaviour{
+
+    public TurretData laserTurretData;
+    public TurretData missileTurretData;
+    public TurretData standardTurretdata;
+
+    //当前选择的炮台
+    private TurretData selectTurretData;
+    public Text moneyText;
+    private int money=1000;
+
+    void ChangeMoney(int change = 0)
+    {
+        money += change;
+        moneyText.text = "￥" + money;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //当点击的不是UI时，进行UI炮台的建设
+            if (EventSystem.current.IsPointerOverGameObject()==false)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//发出一条以摄像机位置为起点，并且穿过鼠标按下的点的 坐标 的射线
+                RaycastHit hit;
+                bool isCollider=Physics.Raycast(ray,out hit, 1000, LayerMask.GetMask("MapCube"));
+                if (isCollider)
+                {
+                    MapCube mapCube = hit.collider.GetComponent<MapCube>();
+                    if (mapCube.turretGo == null)
+                    {
+                        //可以创建
+                        if (money > selectTurretData.cost)
+                        {
+                            mapCube.BuildTurret(selectTurretData.TurretPerfab);
+                            ChangeMoney(-selectTurretData.cost);
+                        }
+                        else
+                        {
+                            //TODO 提示钱不够
+                        }
+                    }
+                    else
+                    {
+                        //TODO 升级处理
+
+
+                    }
+                    //Debug.Log(mapCube);
+                }
+            }
+        }    
+    }
+
+    public void OnLaserSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectTurretData = laserTurretData;
+        }
+    }
+
+    public void OnMissileSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectTurretData = missileTurretData;
+        }
+    }
+
+    public void OnStandardSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectTurretData = standardTurretdata;
+        }
+    }
+
+}
---
+++ Assets/Script/MapCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCube : MonoBehaviour {
+    [HideInInspector]
+    public GameObject turretGo;
+
+    public void BuildTurret(GameObject turretPrefab)
+    {
+
+    }
+}
